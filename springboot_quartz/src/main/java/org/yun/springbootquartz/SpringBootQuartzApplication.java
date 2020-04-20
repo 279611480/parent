7a62af1c654d05ca0dc8f84cabcbee6a13d90729
279611480 <279611480@qq.com>
2020-04-21 00:15:45 +0800

    定时任务demo

---
+++ springboot_quartz/src/main/java/org/yun/springbootquartz/SpringBootQuartzApplication.java
@@ -0,0 +1,21 @@
+package org.yun.springbootquartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName SpringBootQuartzApplication
+ * @Description TODO
+ * @Autor 落笔丶
+ * @Date 2020/4/20 23:49
+ * @Tel 279611480@qq.com
+ */
+@SpringBootApplication
+public class SpringBootQuartzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootQuartzApplication.class, args);
+    }
+
+
+}
